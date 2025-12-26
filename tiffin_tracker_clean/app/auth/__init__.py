6a9f9b9cc4d12d7b2bc9e2ab953ec0e6aeb7e2da
TusharQ15 <dubetushar149@gmail.com>
2025-12-26 21:38:26 +0530

    chore: remove unrelated files and update .gitignore

--- tiffin_tracker_clean/app/auth/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-
-# Create auth blueprint
-# The url_prefix will be added to all routes in this blueprint
-auth = Blueprint('auth', __name__)
-
-# Import views at the bottom to avoid circular imports
-from . import views
